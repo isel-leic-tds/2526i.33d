eff961f90aa7b76be0ce02da08bf8f0033d2015f
Pedro Pereira <593033+palex65@users.noreply.github.com>
2025-09-19 15:47:57 +0100

    TDD - Date 2
    No intervalo da aula 19 setembro

--- Date/src/main/kotlin/Date.kt
+++ Date/src/main/kotlin/Date.kt
@@ -1,9 +1,17 @@
 
+const val GREGORIAN_YEAR = 1582
+const val MAX_YEAR = 2200
+
 class Date(
     val year: Int = 2025,
     val month: Int = 1,
     val day: Int = 1
 ) {
+    init {
+        require(year in GREGORIAN_YEAR..MAX_YEAR) { "Invalid year $year" }
+        require(month in 1..dayOfMonth.size) { "Invalid month $month" }
+        require(day in 1..lastDayOfMonth) { "Invalid day $day" }
+    }
     //constructor(): this(2025,1,1)
     //val leapYear: Boolean get() = year.isLeapYear
 }
@@ -11,12 +19,35 @@ class Date(
 private val dayOfMonth = listOf(31,28,31,30,31,30,31,31,30,31,30,31)
 
 val Date.lastDayOfMonth: Int
-    get() = when(month) {
-        2 -> if (leapYear) 29 else 28
-        4,6,9,11 -> 30
-        else -> 31
-    }
+    get() = if (month==2 && leapYear) 29 else dayOfMonth[month-1]
 
 val Date.leapYear: Boolean get() = year.isLeapYear
 
 val Int.isLeapYear get() = this%4==0 && this%100!=0 || this%400==0
+
+operator fun Date.plus(days: Int): Date = this.addDays(days)
+operator fun Int.plus(date: Date): Date = date.addDays(this)
+
+tailrec fun Date.addDays(d :Int): Date = when {
+    day + d <= lastDayOfMonth ->
+        Date(year, month, day + d)
+    month < dayOfMonth.size ->
+        Date(year, month + 1, 1).addDays(d - (lastDayOfMonth - day + 1))
+    else ->
+        Date(year + 1, 1, 1).addDays(d - (lastDayOfMonth - day + 1))
+}
+
+
+tailrec fun fact(n: Int, r: Int = 1): Int =
+    if (n <= 1) r
+    else fact(n-1,r*n)
+
+
+
+
+
+
+
+
+
+
--- Date/src/test/kotlin/DateTests.kt
+++ Date/src/test/kotlin/DateTests.kt
@@ -37,4 +37,31 @@ class DateTests {
         val sut = Date(2025,9)
         assertEquals(30,sut.lastDayOfMonth)
     }
+    @Test fun `create invalid date`() {
+        val ex = assertFailsWith<IllegalArgumentException> {
+            Date(2025,0)
+        }
+        assertEquals("Invalid month 0",ex.message)
+    }
+    @Test fun `Add days to Date in same month`() {
+        val sut = Date(2025,9,30) + 2
+        // a + b  -> a.plus(b)
+        assertEquals(2,sut.day)
+        assertEquals(10,sut.month)
+        assertEquals(2025,sut.year)
+    }
+    @Test fun `Add days to Date in other year`() {
+        val sut = Date(2025,12,30) + 2
+        // a + b  -> a.plus(b)
+        assertEquals(1,sut.day)
+        assertEquals(1,sut.month)
+        assertEquals(2026,sut.year)
+    }
+
+    @Test fun factorial() {
+        assertEquals(1,fact(1))
+        assertEquals(2,fact(2))
+        assertEquals(6,fact(3))
+        assertEquals(24,fact(4))
+    }
 }
